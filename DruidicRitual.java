184d8c132bde742c31d55f7d86ea202c046b94e3
sslippi <aaronhellmann10@gmail.com>
2020-11-02 01:36:37 -0500

    Updated

--- DruidicRitual.java
+++ DruidicRitual.java
@@ -7,12 +7,14 @@ import org.tribot.api.types.generic.Condition;
 import org.tribot.api2007.Banking;
 import org.tribot.api2007.ChooseOption;
 import org.tribot.api2007.Equipment;
+import org.tribot.api2007.Game;
 import org.tribot.api2007.Equipment.SLOTS;
 import org.tribot.api2007.Skills.SKILLS;
 import org.tribot.api2007.Inventory;
 import org.tribot.api2007.NPCChat;
 import org.tribot.api2007.NPCs;
 import org.tribot.api2007.Objects;
+import org.tribot.api2007.Options;
 import org.tribot.api2007.Player;
 import org.tribot.api2007.Skills;
 import org.tribot.api2007.WebWalking;
@@ -38,18 +40,20 @@ public class DruidicRitual extends Script implements Loopable {
 
 	public ABCUtil abc;
 
-	boolean banking;
-	boolean hasTeleported;
-	boolean hasWalkedToKaqemeex;
-	boolean hasTalkedToKaqemeex;
+	boolean banking1 = true;
+	boolean banking2 = true;
+	boolean hasTeleported = true;
+	boolean hasWalkedToKaqemeex = true;
+	boolean hasTalkedToKaqemeex = true;
 	boolean hasWalkedToSanfew;
-	boolean hasTalkedToSandew;
+	boolean hasTalkedToSanfew;
 	boolean hasWalkedToCauldron;
 	boolean hasInteractedWithCauldron;
 	boolean hasReturnedToSanfew;
 	boolean hasReturnedToKaqemeex;
 
 	RSArea druidsCircle = new RSArea(new RSTile(2922, 3486, 0), new RSTile(2929, 3481, 0));
+	RSArea sanfewsRoom = new RSArea(new RSTile(2897, 3431, 0), new RSTile(2899, 3425, 0));
 
 	@Override
 	// Initial method run by all TriBot Scripts. Executes onStart, and then begins
@@ -68,30 +72,26 @@ public class DruidicRitual extends Script implements Loopable {
 
 	@Override
 	public void onStart() {
-		// TODO Auto-generated method stub
 		println("Hello world.");
 
 	}
 
 	@Override
 	public int onLoop() {
-		// TODO Auto-generated method stub
 		switch (getState()) {
 
 		case BANKING:
 
-			// If 'Games necklace(8)' is not equipped and Quest items aren't in inventory,
-			// then pull the items from the bank and equip 'Games necklace(8)'.
+			if (!banking1) {
 
-			if (!banking) {
+				println("Getting quest items from the bank.");
 
-				println("Getting quest items.");
-
-				// Open bank and wait until it's loaded.
 				if (!Banking.isBankLoaded()) {
+
 					Banking.openBank();
 					Banking.waitUntilLoaded(3000);
-					General.random(100, 300);
+					General.random(300, 600);
+
 				}
 
 				else if (Banking.isBankScreenOpen()) {
@@ -99,42 +99,68 @@ public class DruidicRitual extends Script implements Loopable {
 					int[] itemID = { 2138, 2136, 2134, 2132, 3853, 12625, 88, 11133 };
 					int[] itemID2 = { 8007, 22795 };
 
-					// Withdraws quest items using IDs.
-
 					withdrawItems(itemID, 1);
 
 					if (!Timing.waitCondition(new Condition() {
+
 						@Override
 						public boolean active() {
+
 							General.sleep(200);
 							return (itemCheck(itemID, 1));
+
 						}
-					}, General.random(20000, 40000))) {
-					}
-					;
+
+					}, General.random(20000, 40000)))
+						;
 
 					withdrawItems(itemID2, 5);
 
 					if (!Timing.waitCondition(new Condition() {
 						@Override
 						public boolean active() {
+
 							General.sleep(200);
 							return (itemCheck(itemID2, 5));
+
 						}
-					}, General.random(20000, 40000))) {
-					}
-					;
+					}, General.random(20000, 40000)))
+						;
 
-					General.sleep(10000, 20000);
+					General.sleep(General.randomSD(15000, 3000));
+
 					if (itemCheck(itemID, 1) && itemCheck(itemID2, 5)) {
-						banking = true;
-					}
 
-					if (banking) {
+						banking1 = true;
 						Banking.close();
 						General.sleep(500, 1000);
 
 					}
+
+				}
+
+			}
+
+			if (banking1) {
+
+				println("Equipping Boots of lightness and Regen bracelet");
+
+				if (Inventory.find(88).length > 0 && Inventory.find(11133).length > 0) {
+
+					RSItem[] Boots = Inventory.find(88);
+					RSItem[] Braclets = Inventory.find(11133);
+
+					Boots[0].click("Wear");
+					General.sleep(300, 600);
+					Braclets[0].click("Wear");
+					General.sleep(300, 600);
+
+					if (Inventory.find(88).length < 1 && Inventory.find(11133).length < 1) {
+
+						banking2 = true;
+
+					}
+
 				}
 
 			}
@@ -145,15 +171,17 @@ public class DruidicRitual extends Script implements Loopable {
 
 			if (!hasTeleported) {
 
+				println("Teleporting to Burthorpe.");
+
 				RSItem[] GamesNecks = Inventory.find(gamesNecklace);
 				RSItem GamesNeck = GamesNecks[0];
+
 				GamesNeck.click("Rub");
 
 				General.sleep(5000);
 
 				String[] Options = NPCChat.getOptions();
 
-				println(Options[0]);
 				NPCChat.selectOption("Burthorpe", true);
 
 				General.sleep(5000, 10000);
@@ -166,13 +194,14 @@ public class DruidicRitual extends Script implements Loopable {
 					break;
 			}
 
-			if (!druidsCircle.contains(Player.getPosition())) {
+			if (!druidsCircle.contains(Player.getPosition()) && hasTeleported) {
 
-				
-				println(Player.getPosition());
+				println("Walking to Kaqemeex.");
+
 				WebWalking.walkTo(druidsCircle.getRandomTile());
-				
+
 				General.sleep(5000, 10000);
+
 //				abc.leaveGame();
 
 				if (druidsCircle.contains(Player.getPosition())) {
@@ -188,14 +217,60 @@ public class DruidicRitual extends Script implements Loopable {
 
 		case TALKTOKAQEMEEX:
 
-			RSNPC[] Kaqemeexs = NPCs.findNearest(5045);
-			RSNPC Kaqemeex = Kaqemeexs[0];
-			Kaqemeex.adjustCameraTo();
-			Kaqemeex.click("Talk-to");
+			if (Game.getSetting(80) == 0) {
+
+				println("Talking to Kaqemeex.");
+
+				RSNPC[] Kaqemeexs = NPCs.findNearest(5045);
+				RSNPC Kaqemeex = Kaqemeexs[0];
+				Kaqemeex.adjustCameraTo();
+				Kaqemeex.click("Talk-to");
+				General.sleep(600, 1200);
+				NPCChat.clickContinue(true);
+				General.sleep(300, 600);
+				NPCChat.clickContinue(true);
+				General.sleep(600, 1200);
+				NPCChat.selectOption("I'm in search of a quest.", true);
+				General.sleep(300, 600);
+				NPCChat.clickContinue(true);
+				General.sleep(300, 600);
+				NPCChat.clickContinue(true);
+				General.sleep(300, 600);
+				NPCChat.clickContinue(true);
+				General.sleep(300, 600);
+				NPCChat.clickContinue(true);
+				General.sleep(600, 1200);
+				NPCChat.selectOption("Okay, I will try and help.", false);
+				General.sleep(300, 600);
+				NPCChat.clickContinue(true);
+				General.sleep(300, 600);
+				NPCChat.clickContinue(true);
+				General.sleep(300, 600);
+				NPCChat.clickContinue(true);
+				General.sleep(300, 600);
+			}
+
+			if (Game.getSetting(80) > 0) {
+
+				hasTalkedToKaqemeex = true;
+
+			}
+			;
 
 			break;
 
 		case WALKTOSANFEW:
+
+			if (!sanfewsRoom.contains(Player.getPosition())) {
+				
+				println("Walking to Sanfew.");
+
+				WebWalking.walkTo(sanfewsRoom.getRandomTile());
+
+				if (druidsCircle.contains(Player.getPosition())) {
+					hasWalkedToKaqemeex = true;
+				}
+			}
 
 			break;
 
@@ -253,18 +328,41 @@ public class DruidicRitual extends Script implements Loopable {
 	// Checks if a certain condition is met, then return that state.
 	private State getState() {
 
-		if (!banking) {
+		if (!banking2) {
 			state = State.BANKING;
 		}
 
-		if (!hasWalkedToKaqemeex && banking) {
+		if (!hasWalkedToKaqemeex && banking2) {
 			state = State.WALKTOKAQEMEEX;
 		}
-		
+
 		if (!hasTalkedToKaqemeex && hasWalkedToKaqemeex) {
 			state = State.TALKTOKAQEMEEX;
 		}
 
+		if (!hasWalkedToSanfew && hasTalkedToKaqemeex) {
+			state = State.WALKTOSANFEW;
+		}
+
+		if (!hasTalkedToSanfew && hasWalkedToSanfew) {
+			state = State.TALKTOSANFEW;
+		}
+
+		if (!hasWalkedToCauldron && hasTalkedToSanfew) {
+			state = State.WALKTOCAULDRON;
+		}
+
+		if (!hasInteractedWithCauldron && hasWalkedToCauldron) {
+			state = State.INTERACTWITHCAULDRON;
+		}
+
+		if (!hasReturnedToSanfew && hasInteractedWithCauldron) {
+			state = State.RETURNTOSANFEW;
+		}
+
+		if (!hasReturnedToKaqemeex && hasReturnedToSanfew) {
+			state = State.RETURNTOSANFEW;
+		}
 		return state;
 	}
 
@@ -300,4 +398,22 @@ public class DruidicRitual extends Script implements Loopable {
 		return (false);
 
 	}
+
+/*	public boolean travel(RSArea x, int y, int z) {
+	    while (!x.contains(Player.getPosition())) {
+	        if (//get run energy method < General.random(y, z)) {
+	            Options.setRunOn(false);
+	        } else {
+	        	
+	        	if(!Options.isRunEnabled()) {
+	        	Options.setRunOn(true);
+	        	}
+	        }
+
+	        WebWalking.walkTo(x.getRandomTile());
+
+	        General.randomSD(2500, 500);
+	        General.sleep(2500, 3000);
+	    }
+*/
 }
